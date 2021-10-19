ce233b69439f57ad2b2b98af9a9a879f817aa8b7
EkaterinaBabkina <vozdux@udm.ru>
2021-10-19 21:09:35 +0400

    Added repo on Git

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+
+    @Test
+    void selenideSearchTest() {
+        open("https://www.kinopoisk.ru/");
+
+        $(byName("kp_query")).setValue("Only Murders in the Building").pressEnter();
+
+        $(".styles_link__1dkjp").shouldHave(text("Стив Мартин"));
+    }
+}
