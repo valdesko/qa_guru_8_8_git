530a954edf22ec869595b4e142bc4e8346b55c6b
EkaterinaBabkina <vozdux@udm.ru>
2021-10-20 20:28:29 +0400

    2

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -12,6 +12,6 @@ public class SearchTests {
 
         $(byName("kp_query")).setValue("Only Murders in the Building").pressEnter();
 
-        $(".styles_link__1dkjp").shouldHave(text("Стиaf Мартин"));
+        $(".styles_link__1dkjp").shouldHave(text("Стиafk Мартин"));
     }
 }
